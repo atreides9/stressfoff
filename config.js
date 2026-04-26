ad55f23d79bdb5f3e2e31ad3e35e2c439054e6f6
Summer <lovetung18@gmail.com>
2026-04-26 15:41:28 +0900

    config.js를 git 추적에 포함, presence join/leave 핸들러 추가

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  SUPABASE_URL: 'https://ksxzvjmbbsqllythprdq.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzeHp2am1iYnNxbGx5dGhwcmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MTU0OTMsImV4cCI6MjA5MTQ5MTQ5M30.jDsK5sLJvRf97WQTjCbViHAltZwMDttf993wHVxliM0',
+};
